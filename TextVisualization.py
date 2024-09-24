a9104ea423b6a5bec4d26767f12e4337bce05b9d
Ebjpg <110454033+Ebjpg@users.noreply.github.com>
2024-09-24 18:41:51 +0300

    Wordcloud Generated

--- TextVisualization.py
+++ TextVisualization.py
@@ -2,6 +2,7 @@ import TextPreProcessing
 from TextPreProcessing import df
 import pandas as pd
 import matplotlib.pyplot as plt
+from wordcloud import WordCloud
 
 #Calculate Frequence of "Review"
 tf=df["Review"].apply(lambda x: pd.Series(x.split(" ")).value_counts()).sum(axis=0).reset_index()
@@ -13,3 +14,11 @@ tf.columns = ["words","tf"]
 print(df['Review'])
 tf[tf["tf"]>500].plot.bar(x="words", y="tf")
 plt.show()
+
+#####
+text=" ".join(i for i in df.Review)
+wordcloud=WordCloud().generate(text)
+plt.imshow(wordcloud)
+plt.axis("off")
+plt.show()
+wordcloud.to_file("wordcloud.png")
